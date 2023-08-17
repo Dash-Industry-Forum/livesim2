c73b147cff12c7c6be66de46b80844e79e84fbfb
Torbjörn Einarson <torbjorn@einarssons.se>
2023-08-17 14:54:29 +0200

    feat: HTTP redirect of /livesim and /dash/vod for livesim1 compatibility

--- cmd/livesim2/app/routes.go
+++ cmd/livesim2/app/routes.go
@@ -6,9 +6,19 @@ package app
 
 import (
 	"context"
+	"net/http"
+	"strings"
 
 	"github.com/Dash-Industry-Forum/livesim2/pkg/logging"
 )
+
+// redirect returns an HTTP redirect with "from" replaced by "to" in URL.
+func redirect(from, to string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.URL.Path = strings.Replace(r.URL.Path, from, to, 1)
+		http.Redirect(w, r, r.URL.String(), http.StatusFound)
+	}
+}
 
 // Routes defines dispatches for all routes.
 func (s *Server) Routes(ctx context.Context) error {
@@ -28,5 +38,11 @@ func (s *Server) Routes(ctx context.Context) error {
 	s.VodRouter.MethodFunc("HEAD", "/*", s.vodHandlerFunc)
 	s.Router.MethodFunc("OPTIONS", "/*", s.optionsHandlerFunc)
 	s.Router.MethodFunc("GET", "/", s.indexHandlerFunc)
+	// Redirect /livesim to /livesim2 for backwards compatibility
+	s.Router.MethodFunc("GET", "/livesim/*", redirect("/livesim", "/livesim2"))
+	// Redirect /dash/vod to /vod for backwards compatibility
+	s.Router.MethodFunc("GET", "/dash/vod/*", redirect("/dash/vod", "/vod"))
+	s.Router.MethodFunc("HEAD", "/dash/vod/*", redirect("/dash/vod", "/vod"))
+
 	return nil
 }
