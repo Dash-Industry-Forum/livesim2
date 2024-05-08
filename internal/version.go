4fc654be422d58ef3ec90684084175c78ccf34f5
Torbjörn Einarson <torbjorn@einarssons.se>
2024-05-08 07:14:56 +0200

    docs: v1.3.1

--- internal/version.go
+++ internal/version.go
@@ -12,8 +12,8 @@ import (
 )
 
 var (
-	commitVersion string = "v1.3.0"     // Should be updated during build
-	commitDate    string = "1713872562" // commitDate in Epoch seconds (can be filled/updated in during build)
+	commitVersion string = "v1.3.1"     // Should be updated during build
+	commitDate    string = "1715145165" // commitDate in Epoch seconds (can be filled/updated in during build)
 )
 
 // GetVersion - get version, commitHash and  commitDate depending on what is inserted
